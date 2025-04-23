58143166bdc19c27405b65cee3494fba1a488cc7
Dark-Goddess <0906megan64@gmail.com>
2025-04-24 06:11:46 +0800

    Hostinger config

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -27,8 +27,8 @@ class Database extends Config
     public array $default = [
         'DSN'          => '',
         'hostname'     => 'localhost',
-        'username'     => 'root',
-        'password'     => '',
+        'username'     => 'medilink',
+        'password'     => 'gAlrSyYN7i',
         'database'     => 'medilink',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
