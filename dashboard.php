5900ff4198e9c08827fd0f1cad89270b95c4256e
Dark-Goddess <0906megan64@gmail.com>
2025-04-24 14:26:22 +0800

    fix: update logout link in dashboard

--- dashboard.php
+++ dashboard.php
@@ -367,7 +367,7 @@
                 </a>
             </li>
             <li class="nav-item">
-                <a href="./Config/user_api/logout.php" class="nav-link">
+                <a href="./Config/logout.php" class="nav-link">
                     <i class="fas fa-sign-in-alt"></i> Sign Out
                 </a>
             </li>
